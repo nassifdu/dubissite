c85ec3ee81def5c5e8cedb331d95531108923075
nassifdu <dudugrtuio@gmail.com>
2022-11-03 19:32:44 -0300

    html, css e mais ruby

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_222320) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_211744) do
   create_table "series", force: :cascade do |t|
     t.string "nome"
     t.integer "episodios"
     t.float "nota"
     t.boolean "atual"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "turmas", force: :cascade do |t|
+    t.string "nome"
+    t.datetime "inicio"
+    t.datetime "fim"
+    t.float "horas"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
